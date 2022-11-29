0b4b73d8e79abd4518757f102f295a85dc569421
abhijan2402 <abhishek.jangid643@gmail.com>
2022-11-29 16:42:41 +0530

    adding some stuffs

--- src/Screens/Login.js
+++ src/Screens/Login.js
@@ -1,8 +1,23 @@
 import React, { useState } from "react";
 import { Box, Button, TextField, Typography } from "@mui/material";
+import Modal from '@mui/material/Modal';
 
 const Login = () => {
-
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid orange',
+    borderRadius: "10px",
+    boxShadow: 24,
+    p: 4,
+  };
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <div>
       <form>
@@ -27,17 +42,11 @@ const Login = () => {
 
           <TextField margin="normal" id="outlined-basic" label="Email" variant="outlined" size='large' color="warning" style={{ width: "80%", borderRadius: "10px" }} />
           <TextField margin="normal" id="outlined-basic" label="Password" variant="outlined" size='large' color="warning" style={{ width: "80%", borderRadius: "10px" }} />
-
-
-          {/* <TextField margin="normal" type={'text'} variant="outlined" placeholder="Name" /> */}
-          {/* <TextField margin="normal" type={'email'} id="outlined-basic" variant="outlined" placeholder="Email" width="100%" color="warning" style={{ color: "black", width: "80%" }} />
-          <TextField margin="normal" type={'password'} variant="outlined" placeholder="Password" style={{ color: "black", width: "80%" }} /> */}
-
-
           <Button
             sx={{ marginTop: 3, borderRadius: 2, width: "80%", padding: 1 }}
             variant="contained"
             color="warning"
+            onClick={handleOpen}
 
           >Login</Button>
           <Typography style={{ marginTop: "30px", color: "blue" }}>Forgot Password?</Typography>
@@ -45,6 +54,26 @@ const Login = () => {
 
         </Box>
       </form>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Enter the OTP
+          </Typography>
+          <TextField margin="normal" id="outlined-basic" label="Enter 6 diigt OTP" variant="outlined" size='large' color="warning" style={{ width: "100%", borderRadius: "10px" }} />
+          <Button
+            sx={{ marginTop: 3, borderRadius: 2, width: "100%", padding: 1 }}
+            variant="contained"
+            color="warning"
+            onClick={handleOpen}
+
+          >Next</Button>
+        </Box>
+      </Modal>
     </div>
   );
 };
